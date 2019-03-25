81c794a9b2a8e964fe5faceb99218f0f628a7435
Teetertots9 <andrewteeters9@gmail.com>
2019-03-25 10:38:00 +0000

    I should stop with the memes XD

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,9 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("Wolfo is not impressed");
+  $("#demo").html("Seshs in the streets");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="images/mexican_gray_wolf.jpg" />`);
+  $("#doge-image").append(`<img class="img-circle" src="images/Jinkies.jpg" />`);
 }
 
